f820694fddbda0c726479b6537cf367affd9b4a5
VINIPOOH <47758583+VINIPOOH@users.noreply.github.com>
2020-03-09 14:54:01 +0200

    added dynamic customization of user page by role.

--- authorization/src/main/java/ua/testing/authorization/controller/MainController.java
+++ authorization/src/main/java/ua/testing/authorization/controller/MainController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import ua.testing.authorization.entity.RoleType;
 import ua.testing.authorization.entity.User;
 import ua.testing.authorization.service.UserService;
 
